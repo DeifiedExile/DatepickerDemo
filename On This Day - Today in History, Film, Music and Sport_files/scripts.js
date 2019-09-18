59ce04620fd9afdcb6f6e6e722d9b311471025a3
Lucas <lwolfs@my.wctc.edu>
2019-09-18 10:46:08 -0500

    Added datepicker to site

--- On This Day - Today in History, Film, Music and Sport_files/scripts.js
+++ On This Day - Today in History, Film, Music and Sport_files/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function(e){
    $('.search__text').datepicker({
       constrainInput: false,
       maxDate: new Date(Date.now()),
+      changeMonth: true,
+      changeYear: true
 
    });
    // $('.ui-datepicker').draggable();
